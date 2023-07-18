e5cb440702f0d26d0d7830716aae222e8a3012d4
thakur.munish24323 <thakur.munish24323@gmail.com>
2023-07-18 14:34:16 +0200

    removing the not required headers in the enum

--- src/main/java/com/Automation/util/Header.java
+++ src/main/java/com/Automation/util/Header.java
@@ -1,8 +1,6 @@
 package com.Automation.util;
 
 public enum Header {
-        TEXT("text/plain"),
-        HTML("text/html"),
         JSON("application/json"),
         JAVASCRIPT("application/javascript");
         private final String value;
