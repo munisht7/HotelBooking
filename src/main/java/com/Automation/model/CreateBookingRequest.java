fdaff16228ee68b06f8bc7a097cba2947a793c7b
thakur.munish24323 <thakur.munish24323@gmail.com>
2023-07-18 11:27:41 +0200

    adding the getter and setter at class level

--- src/main/java/com/Automation/model/CreateBookingRequest.java
+++ src/main/java/com/Automation/model/CreateBookingRequest.java
@@ -5,36 +5,26 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+@Getter
+@Setter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class CreateBookingRequest {
 
-    @Getter
-    @Setter
     @JsonProperty("firstname")
     private String firstname;
 
-    @Getter
-    @Setter
     @JsonProperty("bookingdates")
     private Object bookingdates;
 
-    @Getter
-    @Setter
     @JsonProperty("lastname")
     private String lastname;
 
-    @Getter
-    @Setter
     @JsonProperty("totalprice")
     private Integer totalprice;
 
-    @Getter
-    @Setter
     @JsonProperty("depositpaid")
     private Boolean depositpaid;
 
-    @Getter
-    @Setter
     @JsonProperty("additionalneeds")
     private String additionalneeds;
 }
