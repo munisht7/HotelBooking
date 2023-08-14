9af1345fafc4bd783f9175489ed6fd27a303e590
thakur.munish24323 <thakur.munish24323@gmail.com>
2023-08-14 12:08:44 +0200

    adding the logs for then() to know the exact response

--- src/main/java/com/Automation/base/BaseSetUp.java
+++ src/main/java/com/Automation/base/BaseSetUp.java
@@ -12,113 +12,141 @@ public class BaseSetUp {
     public Response response = null;
 
     public Response postApi(String url, CreateBookingRequest requestbody, String contentType, HashMap<String, ? extends Object> headerParameters) {
-        response = given().contentType(contentType).
+        response = given().log().all().
+                contentType(contentType).
                 body(requestbody).
                 headers(headerParameters).
                 when().
                 log().all().
                 post(url).
                 then().
+                log().all().
                 extract().response();
         return response;
     }
 
     public Response postApiForAuth(String url, String requestbody, String contentType) {
-        response = given().contentType(contentType).
+        response = given().
+                log().all().
+                contentType(contentType).
                 body(requestbody).
                 when().
                 log().all().
                 post(url).
                 then().
+                log().all().
                 extract().response();
         return response;
     }
 
     public Response postApiForBookingId(String url, String requestbody, String contentType) {
-        response = given().contentType(contentType).
+        response = given().
+                log().all().
+                contentType(contentType).
                 body(requestbody).
                 when().
                 log().all().
                 post(url).
                 then().
+                log().all().
                 extract().response();
         return response;
     }
 
     public Response getApi(String url, String contentType, HashMap<String, ? extends Object> headerParameters) {
-        response = given().contentType(contentType).
+        response = given().
+                log().all().
+                contentType(contentType).
                 headers(headerParameters).
                 when().
                 log().all().
                 get(url).
                 then().
+                log().all().
                 extract().response();
         return response;
     }
 
     public Response getApiWithPathParam(String url, String contentType, HashMap<String, ? extends Object> headerParameters, Integer pathParam) {
-        response = given().contentType(contentType).
+        response = given().
+                log().all().
+                contentType(contentType).
                 headers(headerParameters).
                 when().
                 log().all().
                 get(url, pathParam).
                 then().
+                log().all().
                 extract().response();
         return response;
     }
 
     public Response patchApiWithPathParam(String url, CreateBookingRequest requestbody, String contentType, HashMap<String, ? extends Object> headerParameters, Integer pathParam) {
-        response = given().contentType(contentType).
+        response = given().
+                log().all().
+                contentType(contentType).
                 body(requestbody).
                 headers(headerParameters).
                 when().
                 log().all().
                 patch(url, pathParam).
                 then().
+                log().all().
                 extract().response();
         return response;
     }
 
     public Response patchApiWithPathParamAndWithoutBody(String url, String contentType, HashMap<String, ? extends Object> headerParameters, Integer pathParam) {
-        response = given().contentType(contentType).
+        response = given().
+                log().all().
+                contentType(contentType).
                 headers(headerParameters).
                 when().
                 log().all().
                 patch(url, pathParam).
                 then().
+                log().all().
                 extract().response();
         return response;
     }
 
 
     public Response putApiWithPathParam(String url, CreateBookingRequest requestbody, String contentType, HashMap<String, ? extends Object> headerParameters, Integer pathParam) {
-        response = given().contentType(contentType).
+        response = given().
+                log().all().
+                contentType(contentType).
                 body(requestbody).
                 headers(headerParameters).
                 when().
                 log().all().
                 put(url, pathParam).
                 then().
+                log().all().
                 extract().response();
         return response;
     }
 
     public Response deleteApi(String url, String contentType, HashMap<String, ? extends Object> headerParameters, Integer pathParam) {
-        response = given().contentType(contentType).
+        response = given().
+                log().all().
+                contentType(contentType).
                 headers(headerParameters).
                 when().
                 log().all().
                 delete(url, pathParam).
                 then().
+                log().all().
                 extract().response();
         return response;
     }
 
     public Response getApiwithQueryParam(String url, String contentType, Map<String, ? extends Object> headerParameters, Map<String, ? extends Object> queryParam) {
-        response = given().contentType(contentType).
+        response = given().
+                log().all().
+                contentType(contentType).
                 headers(headerParameters).
                 queryParams(queryParam).when().log().all()
-                .get(url).then().extract().response();
+                .get(url).then().log().all().extract().response();
         return response;
     }
 }
